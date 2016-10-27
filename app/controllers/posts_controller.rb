7b6759272a896673400865bf356827069798935b
Adam Romeo <adamd.romeo09@gmail.com>
2016-10-27 10:46:57 -0700

    Fixing issues: Posts show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-     @posts = Post.all.order(:likes)
+     @posts = Post.group(session[:user_id]).order('posts_liked DESC')
      @user = User.find(params[:id])
   end
 
