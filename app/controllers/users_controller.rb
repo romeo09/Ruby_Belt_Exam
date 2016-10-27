aa12f041e5a3636f297b605fd402019701ed780f
Adam Romeo <adamd.romeo09@gmail.com>
2016-10-27 09:32:29 -0700

    Updating show all posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
      @user = User.find(params[:id])
-     @posts = Post.all
+     @posts = Post.where(user: @user).all
      @posts_liked = @user.posts_liked.count
   end
   def create
