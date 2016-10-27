f8dccf836f9e3150d8117264b904c8532563630e
Adam Romeo <adamd.romeo09@gmail.com>
2016-10-27 09:40:16 -0700

    Updating show all posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
    before_action :require_login, except: [:new, :create]
-   before_action :require_correct_user, only: [:show, :destroy]
+   before_action :require_correct_user, only: [:destroy]
   def new
 
   end
